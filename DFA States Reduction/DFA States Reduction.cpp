9f790a124281fde8b3eced31ace2c38da1ea858b
Juan Jose <juanjose.munoz@emast.co>
2025-02-24 14:13:44 -0500

    Modificacion del input de las transiciones para la correcta lectura de las filas

--- DFA States Reduction/DFA States Reduction.cpp
+++ DFA States Reduction/DFA States Reduction.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <sstream>
 #include "Reduccion.h"
+#include <vector>
 
 using namespace std;
 
 int main() {
-    cout << "Ingrese el numero de casos: ";
+    //cout << "Ingrese el numero de casos: ";
     int test_cases;
     set<int> estadosDFA;
     int estados;
@@ -20,13 +21,13 @@ int main() {
    
     while (test_cases--) {
 
-        cout << "Ingrese el numero de estados: ";
+        //cout << "Ingrese el numero de estados: ";
         cin >> estados;
         for (int i = 0; i <= estados; i++) {
             estadosDFA.insert(i);
         }
 
-        cout << "Ingrese el alfabeto del DFA: ";
+        //cout << "Ingrese el alfabeto del DFA: ";
         cin.ignore();
         getline(cin, linea);
         stringstream ss(linea);
@@ -44,7 +45,7 @@ int main() {
         cout << endl;
         */
 
-        cout << "Ingrese los estados finales : "; 
+        //cout << "Ingrese los estados finales : "; 
         getline(cin, linea); 
         stringstream cc(linea);
         int estado;
@@ -59,8 +60,30 @@ int main() {
         }
         */
 
-        cout << "Ingrese la tabla de transiciones (una línea por estado, con " << alfabeto.size() << " valores por línea):\n";
-        
+        //cout << "Ingrese la tabla de transiciones (una línea por estado, con " << alfabeto.size() << " valores por línea):\n";
+        for (int i = 0; i <= estados; i++) {
+            getline(cin, linea); 
+            stringstream ss(linea);
+            int estadoLinea; 
+            ss >> estadoLinea;
+            int estadosTransicion;
+
+            for (auto c : alfabeto) {
+                if(ss >> estadosTransicion) {
+                    transiciones[{estadoLinea, c}] = estadosTransicion;
+                }
+            }
+        }
+
+        /* 
+        for (auto& t : transiciones) {
+            cout << "Estado " << t.first.first << " con simbolo " << t.first.second << " -> Estado destino " << t.second << endl;
+        }
+        */
+
+
+        /* 
+        * Forma antigua de registrar Estados
         for (int i = 0; i <= estados; i++) {
             getline(cin, linea);
             stringstream ss(linea);
@@ -75,28 +98,40 @@ int main() {
             }
            
         }
+        */
 
         DFA M(estadosDFA, alfabeto, 0, transiciones, estadosFinalesDFA);
         map<string, bool> parejas = M.generarParejasEstados();
 
+        /* 
         cout << "Antes del cambio:\n";
         for (const auto& par : parejas) {
             cout << par.first << " -> " << (par.second ? "true" : "false") << endl;
         }
+        */
 
         map<string, bool> parejasMarcadas = Reduccion::MarcarPares(parejas, M);
 
-
+        /* 
         cout << "\nDespués del cambio:\n";
         for (const auto& par : parejasMarcadas) {
             cout << par.first << " -> " << (par.second ? "true" : "false") << endl;
         }
+        */
 
         map<string, bool> parejasFinales = Reduccion::MarcarParesNoMarcados(parejasMarcadas, M);
 
+        /* 
         cout << "\nDespués del cambio Final:\n";
         for (const auto& par : parejasFinales) {
             cout << par.first << " -> " << (par.second ? "true" : "false") << endl;
+        }
+        */
+
+        for (const auto& par : parejasFinales) {
+            if (par.second == false) {
+                cout << "(" << par.first << ") ";
+            }
         }
 
         estadosDFA.clear();
